4acb88894abb3b4906912b5a5255b4ae49d5ba42
Rafael Santos <rafaeldsantos442@gmail.com>
2023-10-20 09:10:49 -0300

    Correcao routes e correcao consulta

--- src/controller/ConsultarController.js
+++ src/controller/ConsultarController.js
@@ -5,11 +5,19 @@ module.exports = {
     async consultar(req, res) {
         if(!req.body.nome || req.body.tipo) {
             const consultar = await tabRegistros.sequelize.query("SELECT * FROM tabRegistros")
-            res.status(200),json({
+            return (
+                res.status(200),json({
                 result: consultar,
                 status: "200"
             })
+            )
         }else {
-            return res.status(401)
+            return (
+                res.status(401).json({
+                    message: "Erro ao consultar",
+                    staus: "401"
+                })
+            )
         }
     }
+}
